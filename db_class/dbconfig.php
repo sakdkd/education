61b74b23ffb3e10485aa12b8a88b4453b458cec5
sakdkd <50192168+sakdkd@users.noreply.github.com>
2019-05-13 22:48:48 +0530

    Create dbconfig.php

---
+++ db_class/dbconfig.php
@@ -0,0 +1,20 @@
+<?php 
+	date_default_timezone_set('Asia/Calcutta'); 
+	$baseurl="http://78.46.117.226/education";
+	$builderbaseurl="http://78.46.117.226/education/admin";
+	$builderlocal=$_SERVER['DOCUMENT_ROOT']."/education";
+	$dbhost = 'localhost';
+	$dbuser = 'kanav';
+	$dbpass = 'kanav123@$';
+	$dbname = 'wp_kapil';
+          global $conn;
+	      $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+       // 
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+		 
+//$nonexcArrroles= array("2","10","12");
+
+
+?>
